46331907f86562d46995d5a2ca8c2f59a50e4733
lox <ed817572@gmail.com>
2026-08-25 00:23:18 -0700

    Add simple review IDs and bulk review deletion

--- src/config/metrics.js
+++ src/config/metrics.js
@@ -2,5 +2,5 @@
 export const homepageMetrics = [
   { value: '0+', label: 'Views generated' },
   { value: '0+', label: 'Total clients' },
-  { value: '0.0/5', label: 'Average star reviews' },
+  { value: '4.5/5', label: 'Average star reviews' },
 ];
