92133eeb4263dcaceec0364d388f44eb97ecb2eb
lox <ed817572@gmail.com>
2026-08-25 21:28:23 -0700

    Games Added

--- src/config/games.js
+++ src/config/games.js
@@ -16,7 +16,7 @@ export const gamesConfig = {
       placeId: '110452937713422',
       name: 'DEADFRONT [BETA]',
       description: 'FIGHT UP TO 400 ZOMBIES AT ONCE!',
-      thumbnail: 'https://tr.rbxcdn.com/180DAY-e70e4949fd012d890b4f20f8e1204d81/768/432/Image/Webp/noFilter',
+      thumbnail: 'https://tr.rbxcdn.com/180DAY-b17a14404dc81cbb0472c22b2e4b20c8/768/432/Image/Webp/noFilter',
       robloxUrl: 'https://www.roblox.com/games/110452937713422/DEADFRONT',
       badge: 'Featured',
       caseStudies: true,
